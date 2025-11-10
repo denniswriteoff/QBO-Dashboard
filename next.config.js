7402c6143b86f0fb70edfccfebef17e7c0138798
Pavlo Ivashchenko <pawloiwanov@gmail.com>
2025-11-10 21:26:30 +0200

    update domains

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'qbo-agent.vercel.app'],
   },
   experimental: {
     serverActions: {
